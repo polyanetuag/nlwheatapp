40c7cfa76b4c9529172e70b26032961da47b98f3
Polyane Tuag <pollyportugues@hotmail.com>
2021-10-24 13:09:12 -0300

    rendering font

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
 
 import { Home } from "./src/screens/Home";
 
 export default function App() {
+  //fazendo o carregamento da fonte Roboto
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return <Home />;
 }
